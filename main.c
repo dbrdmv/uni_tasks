11b654752299f1b92eb9b169301dd0bb7ad9ca4b
dbrdmv <dobrydinmikhail@gmail.com>
2026-09-17 09:27:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+double average(FILE *f);
+double average(FILE *f)
+{
+    double sum = 0., current = 0.;
+    int count = 0;
+
+    while (fscanf(f, "%lf", &current) == 1)
+    {
+        sum += current;
+        count++;
+    }
+    if (count == 0)
+    {
+        printf("File is empty\n");
+        return 0;
+    }
+    return sum / (double)count;
+}
+
+int main(void)
+{
+    FILE *f = fopen("input_data.txt", "r");
+    if (f == NULL)
+    {
+        printf("File error\n");
+        return -1;
+    }
+
+    printf("Answer: %lf\n", average(f));
+    fclose(f);
+    return 0;
+}
